7fe8de72743b196947a8a38c827fbc313c576d36
Jayr Alencar <jayralencarpereira@gmail.com>
2017-08-28 21:12:17 -0300

    Arquivos em caixa de textos

--- Project1/main.h
+++ Project1/main.h
@@ -29,6 +29,8 @@ namespace Project1 {
 			//
 			//TODO: Add the constructor code here
 			//
+			this->getMarcadores();
+			this->getObjects();
 		}
 
 	protected:
@@ -42,21 +44,40 @@ namespace Project1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::Button^  button2;
+
+
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 
 
+
+	private: System::Windows::Forms::ContextMenuStrip^  contextMenuStrip1;
+
+
+
+
 	private: System::Windows::Forms::Button^  button3;
-	private: System::Windows::Forms::DataGridView^  dataGridView1;
-	private: System::Windows::Forms::DataGridView^  dataGridView2;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Panel^  panel2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+	private: System::Windows::Forms::Panel^  panel3;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::RichTextBox^  richTextBox2;
+	private: System::Windows::Forms::Label^  resObjcet;
+	private: System::Windows::Forms::Label^  resMarcador;
+	private: System::ComponentModel::IContainer^  components;
+
+
+
 	protected:
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -65,90 +86,173 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->components = (gcnew System::ComponentModel::Container());
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->contextMenuStrip1 = (gcnew System::Windows::Forms::ContextMenuStrip(this->components));
 			this->button3 = (gcnew System::Windows::Forms::Button());
-			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
-			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->richTextBox2 = (gcnew System::Windows::Forms::RichTextBox());
+			this->resMarcador = (gcnew System::Windows::Forms::Label());
+			this->resObjcet = (gcnew System::Windows::Forms::Label());
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
+			this->panel3->SuspendLayout();
 			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(21, 25);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(165, 36);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Criar novo marcador";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &main::button1_Click);
-			// 
-			// button2
-			// 
-			this->button2->Location = System::Drawing::Point(301, 25);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(165, 36);
-			this->button2->TabIndex = 1;
-			this->button2->Text = L"Selecionar Objeto 3D";
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &main::button2_Click);
 			// 
 			// openFileDialog1
 			// 
 			this->openFileDialog1->DefaultExt = L"obj,3ds,osg";
 			this->openFileDialog1->FileName = L"openFileDialog1";
 			// 
+			// contextMenuStrip1
+			// 
+			this->contextMenuStrip1->Name = L"contextMenuStrip1";
+			this->contextMenuStrip1->Size = System::Drawing::Size(61, 4);
+			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(408, 234);
+			this->button3->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Location = System::Drawing::Point(0, 397);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(165, 36);
-			this->button3->TabIndex = 4;
+			this->button3->Size = System::Drawing::Size(693, 36);
+			this->button3->TabIndex = 10;
 			this->button3->Text = L"Iniciar Aplicação";
 			this->button3->UseVisualStyleBackColor = true;
 			// 
-			// dataGridView1
+			// panel1
 			// 
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(21, 67);
-			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(272, 150);
-			this->dataGridView1->TabIndex = 5;
+			this->panel1->Controls->Add(this->resObjcet);
+			this->panel1->Controls->Add(this->resMarcador);
+			this->panel1->Controls->Add(this->button2);
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(0, 0);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(693, 76);
+			this->panel1->TabIndex = 11;
 			// 
-			// dataGridView2
+			// button2
 			// 
-			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView2->Location = System::Drawing::Point(301, 67);
-			this->dataGridView2->Name = L"dataGridView2";
-			this->dataGridView2->Size = System::Drawing::Size(272, 150);
-			this->dataGridView2->TabIndex = 6;
-			
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Location = System::Drawing::Point(343, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(165, 36);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"Selecionar Objeto 3D";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Location = System::Drawing::Point(12, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(165, 36);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Criar novo marcador";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->label1);
+			this->panel2->Controls->Add(this->richTextBox1);
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Left;
+			this->panel2->Location = System::Drawing::Point(0, 76);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(332, 321);
+			this->panel2->TabIndex = 12;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(9, 3);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(66, 13);
+			this->label1->TabIndex = 9;
+			this->label1->Text = L"Marcadores:";
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(12, 27);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(313, 254);
+			this->richTextBox1->TabIndex = 8;
+			this->richTextBox1->Text = L"";
+			// 
+			// panel3
+			// 
+			this->panel3->Controls->Add(this->label2);
+			this->panel3->Controls->Add(this->richTextBox2);
+			this->panel3->Dock = System::Windows::Forms::DockStyle::Right;
+			this->panel3->Location = System::Drawing::Point(338, 76);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(355, 321);
+			this->panel3->TabIndex = 13;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(16, 11);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(43, 13);
+			this->label2->TabIndex = 10;
+			this->label2->Text = L"Objetos";
+			// 
+			// richTextBox2
+			// 
+			this->richTextBox2->Location = System::Drawing::Point(5, 27);
+			this->richTextBox2->Name = L"richTextBox2";
+			this->richTextBox2->Size = System::Drawing::Size(338, 254);
+			this->richTextBox2->TabIndex = 9;
+			this->richTextBox2->Text = L"";
+			// 
+			// resMarcador
+			// 
+			this->resMarcador->AutoSize = true;
+			this->resMarcador->Location = System::Drawing::Point(13, 57);
+			this->resMarcador->Name = L"resMarcador";
+			this->resMarcador->Size = System::Drawing::Size(0, 13);
+			this->resMarcador->TabIndex = 4;
+			// 
+			// resObjcet
+			// 
+			this->resObjcet->AutoSize = true;
+			this->resObjcet->Location = System::Drawing::Point(343, 56);
+			this->resObjcet->Name = L"resObjcet";
+			this->resObjcet->Size = System::Drawing::Size(0, 13);
+			this->resObjcet->TabIndex = 5;
 			// 
 			// main
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(595, 282);
-			this->Controls->Add(this->dataGridView2);
-			this->Controls->Add(this->dataGridView1);
+			this->ClientSize = System::Drawing::Size(693, 433);
+			this->Controls->Add(this->panel3);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->button3);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
 			this->Name = L"main";
 			this->Text = L"GearAPP - Versão 0.0.1 - Prof. Jayr Alencar";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			this->panel3->ResumeLayout(false);
+			this->panel3->PerformLayout();
 			this->ResumeLayout(false);
-
-			
 
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 		
-		this->getMarcadores();
+		
 		system("mk_patt -cpara=\"Data/camera_para.dat\"");
 		
 	}
@@ -159,11 +263,14 @@ namespace Project1 {
 				System::IO::StreamReader(openFileDialog1->FileName);
 			//if (sr->GetType)
 			System::String^ name = Path::GetFileNameWithoutExtension(openFileDialog1->FileName);
-			
-			System::String^ comando = "osgconvd \"" + openFileDialog1->FileName + "\" \"" + Environment::GetEnvironmentVariable("userprofile")+"/Documents/ObjAR/OSG/"+name+".osg\" && exit > output.msg 2> output.err";
+			System::String^ destino = Environment::GetEnvironmentVariable("userprofile") + "/Documents/ObjAR/OSG/" + name + ".osg";
+			System::String^ comando = "osgconvd \"" + openFileDialog1->FileName + "\" \"" + destino +"\" && exit > output.msg 2> output.err";
 			IntPtr ptrToNativeString = Marshal::StringToHGlobalAnsi(comando);
 			char* nativeString = static_cast<char*>(ptrToNativeString.ToPointer());
 			system(nativeString);		//MessageBox::Show(comando);
+
+			
+			
 			sr->Close();
 		}
 	}
@@ -172,26 +279,19 @@ namespace Project1 {
 		StreamReader^ sr = gcnew StreamReader(Environment::GetEnvironmentVariable("userprofile")+"/Documents/ObjAR/Data/markers.dat");
 		String^ line;
 
-		while ((line = sr->ReadLine()) != nullptr)
-		{
-			if (line->Length > 0 && line != ""){
-				if (line->Substring(0, 1) != "#"){
-					int numMarkres = Int32::Parse(line);
-					for (int i = 0; i < numMarkres; i++){
-						String^ line2;
-						while ((line2 = sr->ReadLine()) != nullptr ){
-							if (line2 != ""){
-								if (line2->Substring(0, 1) != "#"){
-									MessageBox::Show(line2);
-								}	
-							}					
-						}
-						
-					}
-				}
-			}
-		}
+		richTextBox1->Text = sr->ReadToEnd();
 		sr->Close();
 	}
-	};
+
+	private: void getObjects(){
+		StreamReader^ sr = gcnew StreamReader(Environment::GetEnvironmentVariable("userprofile") + "/Documents/ObjAR/Data/objects.dat");
+		String^ line;
+
+		richTextBox2->Text = sr->ReadToEnd();
+		sr->Close();
+	}
+
+	private: System::Void dataGridView1_CellContentClick(System::Object^  sender, System::Windows::Forms::DataGridViewCellEventArgs^  e) {
+	}
+};
 }
